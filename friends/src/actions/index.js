fe00b61baa29999a3447ccb4629aaeaa1a63f6e7
Sydney Crumley <srsimps19@gmail.com>
2019-05-23 17:18:19 -0500

    built add friend reducer and action

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -40,3 +40,21 @@ export const getFriends = () => dispatch => {
       console.log(err);
     });
 };
+
+export const ADD_FRIEND_START = 'ADD_FRIEND_START';
+export const ADD_FRIEND_SUCCESS = 'ADD_FRIEND_SUCCESS';
+export const ADD_FRIEND_FAILURE = 'ADD_FRIEND_FAILURE';
+
+export const addFriend = friend => dispatch => {
+  dispatch({ type: ADD_FRIEND_START });
+  return axios
+    .post('http://localhost:5000/api/friends', friend, {
+      headers: { Authorization: localStorage.getItem('token') }
+    })
+    .then(res => {
+      dispatch({ type: ADD_FRIEND_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: ADD_FRIEND_FAILURE, payload: err.response });
+      })
+};
--- friends/src/reducers/friendsReducer.js
+++ friends/src/reducers/friendsReducer.js
@@ -1,9 +1,16 @@
-import { FETCHING_FRIENDS, SUCCESS_GETTING_FRIENDS } from '../actions'
+import { 
+  FETCHING_FRIENDS, 
+  SUCCESS_GETTING_FRIENDS, 
+  ADD_FRIEND_START, 
+  ADD_FRIEND_SUCCESS, 
+  ADD_FRIEND_FAILURE
+} from '../actions'
 
 const initialState = {
    friends: [],
    fetching: false,
    error: '',
+   addingFriend: false,
   };
 
   export const friendsReducer = (state = initialState, action) => {
@@ -20,6 +27,22 @@ const initialState = {
             error: '',
             friends: action.payload,
             fetching: false
+          }
+        case ADD_FRIEND_START: 
+          return {
+            ...state,
+            addingFriend: true
+          }
+        case ADD_FRIEND_SUCCESS:
+          return {
+            ...state,
+            addingFriend: false,
+            error: '',
+            friends: action.payload
+            };
+        case ADD_FRIEND_FAILURE:
+          return {
+            error: action.payload
           }
         default:
           return state;
