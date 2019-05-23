f2159ebe7161d5c959a36371c0af3e4d3af0ed8e
Sydney Crumley <srsimps19@gmail.com>
2019-05-23 14:48:40 -0500

    built out app funtion

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
 
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Friends List</Link>
+          </li>
+        </ul>
+        <Route path='/login' component={Login} />
+        <PrivateRoute exact path='/protected' component={FriendsList} />
+      </div>
+    </Router>
   );
 }
 
