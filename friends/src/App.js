d70d2f2640acdb58787f1ac2e90740b1585f046f
Sydney Crumley <srsimps19@gmail.com>
2019-05-23 15:16:19 -0500

    took out unordered list out of app

--- friends/src/App.js
+++ friends/src/App.js
@@ -10,14 +10,8 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/protected">Friends List</Link>
-          </li>
-        </ul>
+        <Link to="/login">Login</Link>
+        <Link to="/protected">Friends List</Link>
         <Route path='/login' component={Login} />
         <PrivateRoute exact path='/protected' component={FriendsList} />
       </div>
