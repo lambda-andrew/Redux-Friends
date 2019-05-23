34e122cbc79309fde715f432f109e99f87075054
Sydney Crumley <srsimps19@gmail.com>
2019-05-23 18:22:16 -0500

    styled friends list

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -14,16 +14,16 @@ class FriendsList extends React.Component {
     render() {
         if(this.props.fetching){
             return ( 
-            <div>
+            <div className="loadingPage">
                 {this.props.fetching && <h2>We Are Loading Your Friends Now...</h2>}
             </div>
             )
         }
         return (
-            <div>
-                <div>
+            <div className="friendsListDiv">
+                <div className="friendsCards">
                 {this.props.friends.map(friend => (
-                    <div key={friend.id}>
+                    <div className="friendCard" key={friend.id}>
                         <h4>{friend.name}</h4>
                         <p>{friend.age}</p>
                         <p>{friend.email}</p>
